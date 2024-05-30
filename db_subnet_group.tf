80c88d51189190b26d25e89ae7d54adc876f033e
sohan.satpute <sohan.satpute@ftbtrans.com>
2024-05-30 14:11:59 +0530

    changes in db subnet group

--- db_subnet_group.tf
+++ db_subnet_group.tf
@@ -6,6 +6,6 @@ resource "aws_db_subnet_group" "db_sub_group" {
   name       = "main"
   subnet_ids = data.aws_subnet_ids.available_db_subnet.ids
   tags = {
-    Name = "My DB subnet group"
+    Name = "Sohan DB subnet group"
   }
 }
