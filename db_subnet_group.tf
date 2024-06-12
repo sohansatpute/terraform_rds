eacefc9522d9864d19504066c37395435569fd48
sohan <sohan>
2024-06-12 16:58:39 +0530

    dd

--- db_subnet_group.tf
+++ db_subnet_group.tf
@@ -6,6 +6,6 @@ resource "aws_db_subnet_group" "db_sub_group" {
   name       = "main"
   subnet_ids = data.aws_subnet_ids.available_db_subnet.ids
   tags = {
-    Name = "Sohan DB subnet group-updated-new branch"
+    Name = "Sohan DB subnet group-updated-new branchh"
   }
 }
